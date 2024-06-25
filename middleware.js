8d1c82a7447613e8b2efc691fcaf99b73a17b117
GOLD <Adeniran@Gold-MacBookPros-Pro.local>
2024-06-25 18:00:07 +0100

    restored

--- middleware.js
+++ middleware.js
@@ -1,11 +1,6 @@
 import { auth } from "./app/_lib/auth";
 
-// export const middleware = auth;
-export function middleware(request) {
-  console.log(request);
-
-  return NextResponse.redirect(new URL("/login", request.url));
-}
+export const middleware = auth;
 
 export const config = {
   matcher: ["/account"],
