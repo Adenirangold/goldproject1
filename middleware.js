d302928c0e51ae3f8d20522aad8b638404909114
GOLD <Adeniran@Gold-MacBookPros-Pro.local>
2024-06-25 17:56:39 +0100

    refactored

--- middleware.js
+++ middleware.js
@@ -1,21 +1,10 @@
 import { auth } from "./app/_lib/auth";
 
 // export const middleware = auth;
+export function middleware(request) {
+  console.log(request);
 
-export function middleware(req) {
-  console.log("Middleware executed");
-
-  // Assuming 'auth' is a function that checks authentication
-  const isAuthenticated = auth(req);
-  console.log("Authenticated:", isAuthenticated);
-
-  if (!isAuthenticated) {
-    console.log("Redirecting to login");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  console.log("Proceeding to next response");
-  return NextResponse.next();
+  return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
